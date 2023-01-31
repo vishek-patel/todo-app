a8ebeb4fc2db176a4e95b25e05bf49907bbf763c
Vishek Patel <85006315+vishek-patel@users.noreply.github.com>
2023-01-31 14:59:25 +0530

    Update Todos.jsx

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -10,13 +10,8 @@ export const Todos = ({todos,deleteItems}) => {
 
   return (<div className="todo-conatiner">
 
-<<<<<<< HEAD
     {todoArray.length === 0? <h3 style={{color:"white"}}>"No Todos to display"</h3>:
-=======
-    {todoArray.length === 0? "No Todos to display":
->>>>>>> 818b509d016800da41dc81c4bc982d30de4b1428
     todoArray.map((value) =>{
-
         return <TodoItem title={value.title} desc={value.desc} key={value.id} deleteTodo={deleteItems} />
 
     }
